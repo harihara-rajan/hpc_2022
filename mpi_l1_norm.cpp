92e60e9d9d56feaf7ef53f255ea28df52f806c5e
Hari <harihararajanrm.cms@gmail.com>
2022-03-09 22:56:49 +0100

    l1_norm computation bug sorted out

--- mpi_l1_norm.cpp
+++ mpi_l1_norm.cpp
@@ -7,28 +7,33 @@ using namespace std;
 int main ( int argc, char *argv[] )
 {
    int rank;
-   int n = 10000;
+   int n = 12;
    int np = 4; 
-   vector<float> q(n, 1.432);
-   int chunk = n/np;
-   vector<float> sums(chunk);
-   float l1_norm;
+   // vector<long double> q(n, 1.432);
+   long double q[12] = {0.1,0.1,0.1,0.2,0.2,0.2,0.3,0.3,0.3,0.4,0.4,0.4};
+   long double to_sum[3]; 
+   long double sums[4] = {0};
+
    MPI_Init(&argc,&argv);
    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 
-   sums[rank] = 0;
-   for (int j=0; j<chunk; j++)
+
+   MPI_Scatter(q, 3, MPI_LONG_DOUBLE, to_sum, 3, MPI_LONG_DOUBLE, 0, MPI_COMM_WORLD);
+   
+   for (int j=0; j<3; j++)
    {
-      sums[rank]+= q[j];
+      sums[rank]+= to_sum[j];
    }
 
+   MPI_Gather(&sums[rank],1,MPI_LONG_DOUBLE,sums,1,MPI_LONG_DOUBLE,0,MPI_COMM_WORLD);
    
-   MPI_Reduce(&sums[rank], &l1_norm,1, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
-
    if (rank == 0)
    {
-      cout << l1_norm << "  ";
-      cout << endl;
+      for(int j=1; j<4; j++)
+      {
+         sums[0] += sums[j];
+      }
+      cout << sums[0] << endl;
    }
 
    MPI_Finalize();
