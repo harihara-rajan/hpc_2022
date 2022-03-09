b2afb81ac170b2ee2c505d19ef60c3ae300767ea
Hari <harihararajanrm.cms@gmail.com>
2022-03-09 22:59:56 +0100

    l1 norm computation  generalised

--- mpi_l1_norm.cpp
+++ mpi_l1_norm.cpp
@@ -9,18 +9,19 @@ int main ( int argc, char *argv[] )
    int rank;
    int n = 12;
    int np = 4; 
+   int chunks = n/np;
    // vector<long double> q(n, 1.432);
-   long double q[12] = {0.1,0.1,0.1,0.2,0.2,0.2,0.3,0.3,0.3,0.4,0.4,0.4};
-   long double to_sum[3]; 
-   long double sums[4] = {0};
+   long double q[n] = {0.1,0.1,0.1,0.2,0.2,0.2,0.3,0.3,0.3,0.4,0.4,0.4};
+   long double to_sum[chunks]; 
+   long double sums[np] = {0};
 
    MPI_Init(&argc,&argv);
    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 
 
-   MPI_Scatter(q, 3, MPI_LONG_DOUBLE, to_sum, 3, MPI_LONG_DOUBLE, 0, MPI_COMM_WORLD);
+   MPI_Scatter(q, chunks, MPI_LONG_DOUBLE, to_sum, chunks, MPI_LONG_DOUBLE, 0, MPI_COMM_WORLD);
    
-   for (int j=0; j<3; j++)
+   for (int j=0; j<chunks; j++)
    {
       sums[rank]+= to_sum[j];
    }
@@ -29,7 +30,7 @@ int main ( int argc, char *argv[] )
    
    if (rank == 0)
    {
-      for(int j=1; j<4; j++)
+      for(int j=1; j<np; j++)
       {
          sums[0] += sums[j];
       }
