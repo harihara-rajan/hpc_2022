36de99facfd8e8ee08572c335da8e050f961f2f0
Hari <harihararajanrm.cms@gmail.com>
2022-03-11 14:48:30 +0100

    implemented padding concept in parallelising L

--- mat.cpp
+++ mat.cpp
@@ -9,20 +9,36 @@ int main ( int argc, char *argv[] )
 {
 	time_t walltime = time(nullptr);
     srand(time(NULL));
-    int rank;
-    int n = 12;
-    int np = 4; 
-    int chunks = n/np;
-    int L [n][n]; 
+
+
+    int n_actual = 18;
+    int i, j, rank, size, n;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    int rem = n_actual%size;
+
+    if (rem!=0)
+    {
+        n = n_actual + (size-rem);
+    } 
+
+    else
+    {
+        n = n_actual;
+    }   
+
+    cout << "n =  " << n  << endl;
+    int chunks = n/size;
+
+    int L [n][n]= {0}; 
     int L_sub [chunks][n];
-    MPI_Init(&argc,&argv);  
-    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 
     MPI_Scatter(L, chunks*n, MPI_INT, L_sub, chunks*n, MPI_INT, 0, MPI_COMM_WORLD);
 
     for (int i=0; i<chunks; i++)
     {
-        for (int j=0; j<n; j++)
+        for (int j=0; j<n_actual; j++)
         {
             if (rank == 0)
             {
@@ -30,33 +46,20 @@ int main ( int argc, char *argv[] )
             }
             else
             {
-                L_sub[i][j] = rand()%1;
+                L_sub[i][j] = rand();
+                L_sub[i][j] = rand() % 2;
 
             }
         }
     }
 
-    MPI_Gather(&L_sub[rank],chunks*n, MPI_INT, L_sub, chunks*n, MPI_INT, 0, MPI_COMM_WORLD);
-    // cout << "rank = " << rank << endl;
-    for (int i=0; i<n; i++)
-    {
-        for (int j=0; j<n; j++)
-        {   
-            if (rank == 0)
-            {
-                L[i][j] = L_sub[i][j]; 
-            }
-            else
-            {
-                L[i][j] = L_sub[i][j]; 
-            }
-        }
-    }
+    MPI_Gather(L_sub,chunks*n, MPI_INT, L, chunks*n, MPI_INT, 0, MPI_COMM_WORLD);
+
     if (rank == 0)
     {
-        for (int i=0; i<n; i++)
+        for (int i=0; i<n_actual; i++)
         {
-            for (int j=0; j<n; j++)
+            for (int j=0; j<n_actual; j++)
             {   
                 cout<< L[i][j] << " " ;
             }
