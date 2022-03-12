9f65132574d95472501217fdde7dcf2345b93605
Hari <harihararajanrm.cms@gmail.com>
2022-03-12 11:46:05 +0100

    n_links working for every process completely

--- mat.cpp
+++ mat.cpp
@@ -8,10 +8,10 @@ using namespace std;
 int main ( int argc, char *argv[] )
 {
 	time_t walltime = time(nullptr);
-    srand(time(NULL));
+    // srand(time(NULL));
 
 
-    int n_actual = 7;
+    int n_actual = 5;
     int i, j, rank, size, n;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -29,25 +29,43 @@ int main ( int argc, char *argv[] )
     }   
 
     int chunks = n/size;
-
     int L [n][n]= {0}; 
-    int L_sub [chunks][n];
+    int L_sub [chunks][n] = {0};
     int n_links[n] = {0};
+    int end_process = (n_actual / chunks) ;
+    int rows_end_process = n_actual % chunks;
+
+    if  (rank==0)
+    {
+        cout << "End process : " << end_process << endl;
+        cout << "Rows end    : "  << rows_end_process << endl;
+    }
 
     MPI_Scatter(L, chunks*n, MPI_INT, L_sub, chunks*n, MPI_INT, 0, MPI_COMM_WORLD);
 
     for (int i=0; i<chunks; i++)
     {
-        for (int j=0; j<n_actual; j++)
+        for (int j=0; j<n; j++)
         {
-            if (rank == 0)
+            if (rank ==0)
             {
                 L_sub[i][j] = rand() %2;
             }
             else
             {
-                L_sub[i][j] = rand();
-                L_sub[i][j] = rand() % 2;
+                if (rank==end_process && n!= n_actual && i>= rows_end_process)
+                {
+                    L_sub[i][j] = 0;
+                }
+                else if (rank > end_process)
+                {
+                    L_sub[i][j] = 0;
+                } 
+                else
+                {
+                    L_sub[i][j] = rand();
+                    L_sub[i][j] = rand() % 2;
+                }
 
             }
         }
@@ -67,30 +85,71 @@ int main ( int argc, char *argv[] )
         }
     }
 
-    if (rank ==0)
+    MPI_Scatter(L, n*chunks, MPI_INT, L_sub, n*chunks, MPI_INT, 0, MPI_COMM_WORLD);
+    
+    for (i=0; i<chunks; i++)
     {
-
-        MPI_Scatter(L, chunks*n, MPI_INT, L_sub, chunks*n, MPI_INT, 0, MPI_COMM_WORLD);
-        
-        for (i=0; i<chunks; i++)
+        for (j=0; j<n_actual; j++)
         {
-            for (j=0; j<n_actual; j++)
-            {
-                n_links [i] += L_sub[j][i];
-            }
-        }
-
-        MPI_Gather(&n_links, n, MPI_INT, n_links, n,MPI_INT, 0, MPI_COMM_WORLD);
-
-        if (rank ==0)
-        {
-            for (i=0; i<n_actual; i++)
-            {
-                cout << n_links[i] << " ";
-            }
-            cout << endl;
+            n_links [j] += L_sub[i][j];
         }
     }
+
+    if (rank ==0)
+    {
+        cout << "Rank = " << rank << endl;
+        for (i=0; i<n_actual; i++)
+        {
+            cout << n_links[i] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    if (rank ==1)
+    {
+        cout << "Rank = " << rank << endl;
+        for (i=0; i<n_actual; i++)
+        {
+            cout << n_links[i] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    if (rank ==2)
+    {
+        cout << "Rank = " << rank << endl;
+        for (i=0; i<n_actual; i++)
+        {
+            cout << n_links[i] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    if (rank ==3)
+    {
+        cout << "Rank = " << rank << endl;;
+        for (i=0; i<n_actual; i++)
+        {
+            cout << n_links[i] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+
+
+    MPI_Gather(&n_links, n, MPI_INT, n_links, n,MPI_INT, 0, MPI_COMM_WORLD);
+
+    // if (rank ==0)
+    // {
+    //     for (i=0; i<n_actual; i++)
+    //     {
+    //         cout << n_links[i] << " ";
+    //     }
+    //     cout << endl;
+    // }
 
 
 
